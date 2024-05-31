f6044640ee769aaff53ddf39e8905f3a72a44f92
Pedro <jp.pedrozo@hotmail.com>
2024-05-31 18:20:49 -0300

    fix: busy folder

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-async function replaceUploadsFolder({ sourceDir, destDir }) {
+async function copyUploadsFolder({ sourceDir, destDir }) {
   try {
-    // Remove the destination directory if it exists
-    if (fs.existsSync(destDir)) {
-      await fs.remove(destDir);
-      console.log(`Removed existing directory: ${destDir}`);
-    }
+    // Ensure the destination directory exists
+    await fs.ensureDir(destDir);
 
-    // Copy the source directory to the destination
-    await fs.copy(sourceDir, destDir);
-    console.log(`Copied ${sourceDir} to ${destDir}`);
+    console.log(`Copying contents of ${sourceDir} to ${destDir}`);
+    await fs.copy(sourceDir, destDir, { overwrite: true });
+    console.log(`Copied contents of ${sourceDir} to ${destDir}`);
   } catch (err) {
-    console.error(`Error occurred: ${err}`);
+    if (err.code === "EACCES") {
+      console.error(`Permission error: ${err.message}`);
+    } else if (err.code === "EBUSY") {
+      console.error(`Resource busy error: ${err.message}`);
+    } else {
+      console.error(`Error occurred: ${err.message}`);
+    }
   }
 }
 
@@ -36,11 +39,15 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   bootstrap({ strapi }) {
+    console.log(111);
+
     if (process.env.NODE_ENV === "production") {
+      console.log(222);
+
       const sourceDir = path.join("/", "uploads");
       const destDir = path.join(__dirname, "public", "uploads");
 
-      replaceUploadsFolder({ sourceDir, destDir });
+      copyUploadsFolder({ sourceDir, destDir });
 
       strapi.db.lifecycles.subscribe({
         models: ["plugin::upload.file"],
@@ -48,19 +55,19 @@ module.exports = {
           const sourceDir = path.join(__dirname, "public", "uploads");
           const destDir = path.join("/", "uploads");
 
-          replaceUploadsFolder({ sourceDir, destDir });
+          copyUploadsFolder({ sourceDir, destDir });
         },
         async afterUpdate() {
           const sourceDir = path.join(__dirname, "public", "uploads");
           const destDir = path.join("/", "uploads");
 
-          replaceUploadsFolder({ sourceDir, destDir });
+          copyUploadsFolder({ sourceDir, destDir });
         },
         async afterDelete() {
           const sourceDir = path.join(__dirname, "public", "uploads");
           const destDir = path.join("/", "uploads");
 
-          replaceUploadsFolder({ sourceDir, destDir });
+          copyUploadsFolder({ sourceDir, destDir });
         },
       });
     }
